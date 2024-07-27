2883514f61faf1045bb20300cc2bbeaae69cb4e7
aaksant <aaksangit@gmail.com>
2024-07-27 18:50:25 +0700

    build: add SVG loader

--- webpack.common.js
+++ webpack.common.js
@@ -3,8 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    index: './src/index.js',
-    click: './src/click.js'
+    index: './src/index.js'
   },
   output: {
     filename: '[name].bundle.js',
@@ -16,6 +15,10 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
       }
     ]
   },
