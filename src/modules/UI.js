4c7fc2aeb4386bb4d929f7760c61219f76215661
aaksant <aaksangit@gmail.com>
2024-07-29 21:08:54 +0700

    feat: add base functionality

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,6 +1,7 @@
 import Fetcher from './Fetcher';
 import WeatherData from './WeatherData';
 import Handler from './Handler';
+import icons from './iconLoader';
 
 export default class UI {
   init() {
@@ -19,10 +20,38 @@ export default class UI {
     }
   }
 
+  getSelectedTemperatureMetric() {
+    const currentTempratureMetric = document.querySelector('.current-temp-metric');
+    return currentTempratureMetric.textContent;
+  }
+
   updateUI() {
     const todayData = this.weatherData.getTodayData();
-    
-    const city = document.querySelector('.location-container .city');
+
+    // DOM elements
+    const mainContainer = document.querySelector('.main-container');
+    const city = document.querySelector('.city');
+    const condition = document.querySelector('.condition');
+    const realTemp = document.querySelector('.real-temp');
+    const humidity = document.querySelector('.humidity');
+    const windSpeed = document.querySelector('.wind-speed');
+    const uvIndex = document.querySelector('.uv-index');
+
+    const icon = document.querySelector('.icon');
+    const iconName = todayData.forecast.icon;
+
+    mainContainer.classList.remove('hidden');
     city.textContent = todayData.city;
+    condition.textContent = todayData.forecast.conditions;
+    realTemp.textContent = todayData.forecast.temp;
+    humidity.textContent = todayData.forecast.humidity;
+    windSpeed.textContent = todayData.forecast.windspeed;
+    uvIndex.textContent = todayData.forecast.uvindex;
+
+    if (icons[iconName]) {
+      icon.src = icons[iconName];
+    } else {
+      alert(`Cannot find icond: ${iconName}`);
+    }
   }
 }
