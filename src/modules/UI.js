ae65f9ead93fa599a3b21fd219727721a3e1ddf2
aaksant <aaksangit@gmail.com>
2024-07-29 23:15:38 +0700

    feat: add change temperature unit feature

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -2,12 +2,14 @@ import Fetcher from './Fetcher';
 import WeatherData from './WeatherData';
 import Handler from './Handler';
 import icons from './iconLoader';
+import { convertTemperature } from './utils';
 
 export default class UI {
-  init() {
+  constructor() {
     this.fetcher = new Fetcher();
     this.weatherData = new WeatherData();
     this.handler = new Handler(this);
+    this.isFahrenheit = true;
   }
 
   async updateWeather(city) {
@@ -20,15 +22,31 @@ export default class UI {
     }
   }
 
-  getSelectedTemperatureMetric() {
-    const currentTempratureMetric = document.querySelector('.current-temp-metric');
-    return currentTempratureMetric.textContent;
+  changeTemperatureUnit() {
+    this.isFahrenheit = !this.isFahrenheit;
+    this.updateUI();
+  }
+
+  setTemperatureType(type) {
+    this.currentTempType = type;
+    this.updateUI();
+  }
+
+  loadIcon(data) {
+    const icon = document.querySelector('.icon');
+    const iconName = data.forecast.icon;
+
+    if (icons[iconName]) {
+      icon.src = icons[iconName];
+    } else {
+      alert(`Cannot find icon: ${iconName}`);
+    }
   }
 
   updateUI() {
     const todayData = this.weatherData.getTodayData();
+    this.loadIcon(todayData);
 
-    // DOM elements
     const mainContainer = document.querySelector('.main-container');
     const city = document.querySelector('.city');
     const condition = document.querySelector('.condition');
@@ -37,21 +55,21 @@ export default class UI {
     const windSpeed = document.querySelector('.wind-speed');
     const uvIndex = document.querySelector('.uv-index');
 
-    const icon = document.querySelector('.icon');
-    const iconName = todayData.forecast.icon;
+    const currentTempUnit = document.querySelector('.temp-unit');
+    const tempValue = +convertTemperature(
+      todayData.forecast.temp,
+      this.isFahrenheit
+    );
+    const unit = this.isFahrenheit ? 'F' : 'C';
 
     mainContainer.classList.remove('hidden');
+    currentTempUnit.textContent = this.isFahrenheit ? 'Fahrenheit' : 'Celcius';
+
     city.textContent = todayData.city;
     condition.textContent = todayData.forecast.conditions;
-    realTemp.textContent = todayData.forecast.temp;
+    realTemp.textContent = `${tempValue.toFixed(2)}°${unit}`;
     humidity.textContent = todayData.forecast.humidity;
     windSpeed.textContent = todayData.forecast.windspeed;
     uvIndex.textContent = todayData.forecast.uvindex;
-
-    if (icons[iconName]) {
-      icon.src = icons[iconName];
-    } else {
-      alert(`Cannot find icond: ${iconName}`);
-    }
   }
 }
