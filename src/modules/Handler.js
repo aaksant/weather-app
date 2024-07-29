60b1da40325155975781a97a7fd221f82d781c34
aaksant <aaksangit@gmail.com>
2024-07-29 23:16:34 +0700

    feat: add temperature unit change handler

--- src/modules/Handler.js
+++ src/modules/Handler.js
@@ -2,6 +2,7 @@ export default class Handler {
   constructor(ui) {
     this.ui = ui;
     this.handleInput();
+    this.handleTemperatureUnitChange();
   }
 
   handleInput() {
@@ -11,6 +12,16 @@ export default class Handler {
     form.addEventListener('submit', e => {
       e.preventDefault();
       this.ui.updateWeather(input.value);
+    });
+  }
+
+  handleTemperatureUnitChange() {
+    const tempToggle = document.querySelector('.temp-unit');
+    const mainContainer = document.querySelector('.main-container');
+
+    tempToggle.addEventListener('click', () => {
+      if (mainContainer.classList.contains('hidden')) return;
+      this.ui.changeTemperatureUnit();
     });
   }
 }
